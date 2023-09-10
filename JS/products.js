d3a7cc92529ce44db7d7e1155e751797c7ebc1ba
TheresiaAurelly <theresiaAurelly@gmail.com>
2023-09-10 19:33:12 +0700

    resolve filtering menu

--- JS/products.js
+++ JS/products.js
@@ -1,22 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const tabs = document.querySelectorAll(".category-option");
+    const menuItems = document.querySelectorAll(".category-option");
     const coffeeItems = document.querySelectorAll(".menu-item");
 
-    tabs.forEach((tab) => {
-        tab.addEventListener("click", () => {
-            tabs.forEach((t) => t.classList.remove("active"));
-            tab.classList.add("active");
+    menuItems.forEach((menuItem) => {
+        menuItem.addEventListener("click", function (event) {
+            event.preventDefault();
 
-            const filter = tab.getAttribute("data-filter");
+            // Hapus kelas "active" dari semua item menu
+            menuItems.forEach((item) => item.classList.remove("active"));
+            // Tambahkan kelas "active" hanya pada item menu yang diklik
+            this.classList.add("active");
 
-            coffeeItems.forEach((item) => {
-                const category = item.getAttribute("data-category");
-                if (filter === "all" || category === filter) {
-                    item.style.display = "block";
+            // Dapatkan nilai data-filter dari item menu yang diklik
+            const filter = this.getAttribute("data-filter");
+
+            // Tampilkan atau sembunyikan item kopi berdasarkan filter
+            coffeeItems.forEach((coffeeItem) => {
+                const category = coffeeItem.getAttribute("data-category");
+                if (filter === "all" || filter === category) {
+                    coffeeItem.style.display = "block";
                 } else {
-                    item.style.display = "none";
+                    coffeeItem.style.display = "none";
                 }
             });
         });
     });
 });
+
