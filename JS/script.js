45289acf36dcd810779f7cf0df16d7f69918e5a1
ImamDinnullah <buku.usangku1@gmail.com>
2023-09-29 10:52:40 +0700

    membuat halaman checkout.html

--- JS/script.js
+++ JS/script.js
@@ -18,31 +18,3 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navList.classList.remove("active");
   })
 );
-
-// Dropdown Checkout.html
-/* When the user clicks on the button, 
-        toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (e) {
-  if (!e.target.matches(".dropbtn")) {
-    var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains("show")) {
-      myDropdown.classList.remove("show");
-    }
-  }
-};
-// close function dropdown
-
-/* 
-const navbarToggle = document.getElementById('navbar-toggle');
-const navbarMenu = document.querySelector('.navbar-menu');
-
-navbarToggle.addEventListener('click', ()=> {
-  navbarToggle.classList.toggle('active');
-  navbarMenu.classList.toggle('active');
-});
-*/
