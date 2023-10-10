1a60332724a6feeac0c4a5f5cb8b9d4a55a71b8c
Muhammad Maulana <muhmaulanaa@tutanota.com>
2023-10-10 09:51:00 +0700

    Penambahan alert di checkout

--- JS/checkout.js
+++ JS/checkout.js
@@ -34,8 +34,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Tambahkan event listener untuk menangani klik pada tombol Checkout
   checkoutButton.addEventListener("click", function (event) {
-    // Tampilkan pesan alert
-    alert("Your order has been received");
-    window.location.href = "products.html";
+    // Dapatkan elemen-elemen input yang perlu divalidasi
+    const fullNameInput = document.querySelector("#fname");
+    const emailInput = document.querySelector("#email");
+    const addressInput = document.querySelector("#adr");
+    const transferBankInput = document.querySelector("#transferbank");
+    const expeditionInput = document.querySelector("#expedition");
+
+    // Validasi formulir secara manual
+    if (
+      fullNameInput.value === "" ||
+      emailInput.value === "" ||
+      addressInput.value === "" ||
+      transferBankInput.value === "" ||
+      expeditionInput.value === ""
+    ) {
+      // Jika ada elemen yang belum diisi, tampilkan peringatan
+      alert("Please fill out all the required fields before checking out.");
+    } else {
+      // Jika semua input terisi, tampilkan pesan alert
+      alert("Your order has been received");
+      window.location.href = "products.html";
+    }
+
+    // Mencegah perilaku default dari tombol "Checkout"
+    event.preventDefault();
   });
 });
